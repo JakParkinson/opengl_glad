704fb47a07d7e492050f40fd95f7f45cc75c2a8c
jrongtt <jackparkinson1394@gmail.com>
2025-09-04 11:01:33 -0700

    assimp

---
+++ assimp_test/main.cpp
@@ -0,0 +1,23 @@
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "../stb/stb_image.h"
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "shader_m.h"
+#include "camera.h"
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "oh yeahhhhh" << std::endl; 
+    return 0;
+}
